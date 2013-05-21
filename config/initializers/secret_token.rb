84da24d59c3db39d9018492747484f9b90312feb
Gyu <gyukwon@gmail.com>
2013-05-21 15:35:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AhcJbDemo::Application.config.secret_token = 'd58870b7941d5d29119ee46da2f2bbb6340af7550106cf79d7135bf6d1f0fa5256b78de0ff6419417d8dffa69cb9bff20c3e069f12b8a37daaf65689356067f5'
